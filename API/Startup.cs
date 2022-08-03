f4a6414e68121fd5a643095b3174922618558613
Andrew Lewis <andrew.lewis@clinicalcomputersystems.com>
2022-08-03 15:34:20 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,6 +45,13 @@ namespace API
             {
                 Opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +67,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
